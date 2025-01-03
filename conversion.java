0f8dcdeff85de007f1fb55a2cce33ef5d4294ea3
MRITUNJAY KUMAR <mritunjay1333@gmail.com>
2025-01-03 19:36:20 +0530

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,71 @@
+package com.conversion;
+
+public class conversion {
+
+	public static void main(String[] args) {	
+		int a=10;
+		byte b=(byte)a;
+		System.out.println(b);
+		byte a1=10;
+		
+		int b1=a;
+		
+		System.out.println(b1);
+	    char a2='A';
+	    int b2=a2;
+	    System.out.println(b2);
+	    
+	     float f1=98.344f;
+	       byte b3=(byte)f1;
+	       System.out.println(b3);
+
+	      // int i1=999;
+	      // boolean b4=boolean(i1);
+	      // System.out.println(i1);
+	        
+	       int i2=98;
+	       char ch=(char)i2;
+	       System.out.println(i2);
+	       
+	       double d1=98987898;
+	       int i3=(int)d1;
+	       System.out.println(d1);
+	       byte b4=10;
+	       short s1=b4;
+	       System.out.println(s1);
+	       
+	       float f2=98.99f;
+	       short s2=(short)f2;
+	       System.out.println(s2);
+	       double d4=99.99;
+	       char ch1=(char)d4;
+	       System.out.println(ch1);
+	  // boolean b5=true;
+	 //  short s3=(short)b5; we cannot convert boolean to any other types OR
+	       //any other types can not be converted into boolean.
+	  // System.out.println(s3);
+	     //  int i5=9876;
+	     //  String s6=(String)i5;
+	      // System.out.println(s6);
+	       char ch3='C';
+	       int i5=ch3;
+	       System.out.println(i5);
+	       
+	       int i9=99;
+	       char ch11=(char)i9;
+	       System.out.println(ch11);
+	       
+	       float f12=99.00f;
+	       double d12=f12;
+	       System.out.println(d12);
+	       
+	        char ch12='C';
+	        int i12=ch12;
+	        System.out.println(ch12);
+	        
+	        long L13=989898989898l;
+	        int i13=(int)L13;
+	        System.out.println(i13);
+	        
+}
+}
